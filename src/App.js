9423148f707a51bfd87699cf2e50dbca0f221398
Zerrin Ekici <zerkam06@gmail.com>
2020-12-30 21:03:00 -0600

    add react css project day 22 file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import '../App.css'  
+function App () {
+return (
+  <div className="parent">
+    <h1 className ="title">Hello World!</h1>
+    <h2 className="child">How are you?</h2>
+    <p id="parap">I'm fine!</p>
+  </div>
+ 
+)
+}
